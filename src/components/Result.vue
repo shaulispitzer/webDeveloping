faec7dba2ff054c8aa7066935088436677b8cfe3
Shauli Spitzer <sluahs@gmail.com>
2023-05-15 09:58:23 +0300

    added confetti

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,4 +1,29 @@
+<script setup>
+import { onMounted } from 'vue';
+import Confetti from 'canvas-confetti';
+onMounted(()=>{
+    const end = Date.now() + 3 * 1000
 
+    // go Buckeyes!
+    const colors = ["#d61f69", "#ff99ff","#bada55","#00c1ff"]
+
+    ;(function frame() {
+        Confetti({
+            particleCount: 20,
+            angle: 100,
+            spread: 100,
+            startVelocity: 50,
+            colors: colors,
+            drift:1,
+            scalar: 1
+        })
+
+        if(Date.now() <end){
+            requestAnimationFrame(frame)
+        }
+    })()
+})
+</script>
 <template>
         <div class="resultMessage p-4 my-3 mx-auto sm:w-80 w-[90%] min-w-max border-2 border-green-700 rounded-lg bg-green-300">
     <p class="text-lg font-bold uppercase">your number is:</p>
