5af7568ed55349bd256f24fa38da47b48e96295e
Shauli Spitzer <sluahs@gmail.com>
2023-08-13 16:47:18 +0300

    change from front-end to full-stack

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
-import WebsiteDesign from './components/WebsiteDesign.vue';
-import Education from './components/Education.vue';
-import Fluency from './components/Fluency.vue';
-import projects from './components/projects.vue';
-import contactAndPhoto from './components/contactAndPhoto.vue';
+import WebsiteDesign from "./components/WebsiteDesign.vue";
+import Education from "./components/Education.vue";
+import Fluency from "./components/Fluency.vue";
+import projects from "./components/projects.vue";
+import contactAndPhoto from "./components/contactAndPhoto.vue";
 </script>
 
 <template>
   <div class="w-full bg-blue py-4">
-    <p class="font-semibold md:text-4xl text-3xl text-center pb-2">Shauli Spitzer</p>
-    <p class="font-semibold md:text-3xl text-2xl text-center">Front-End Web Developer</p>
+    <p class="font-semibold md:text-4xl text-3xl text-center pb-2">
+      Shauli Spitzer
+    </p>
+    <p class="font-semibold md:text-3xl text-2xl text-center">
+      Full-Stack Web Developer
+    </p>
   </div>
   <WebsiteDesign></WebsiteDesign>
   <Education></Education>
@@ -18,6 +22,4 @@ import contactAndPhoto from './components/contactAndPhoto.vue';
   <contactAndPhoto></contactAndPhoto>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/Fluency.vue
+++ src/components/Fluency.vue
@@ -1,14 +1,22 @@
 <template>
-    <div class="bg-slate-700 h-auto p-7 flex justify-center">
-        <div class="h-full md:flex p-4">
-            <SlideIn left>
-                <div class="my-auto md:px-5 px-3"><p class="text-white italic text-xl mb-2">Fluent in HTML, CSS, JavaScript, Vue.js and Tailwind CSS</p><hr>
-                    <p class="text-white mb:p-0 py-2">Proficient in creating responsive and dynamic user interfaces using modern web development technologies</p>
-                </div>
-            </SlideIn>
-   <SlideIn><img src="/photo-2.webp" alt="" class="h-64 mx-auto"></SlideIn>
-    </div></div>
+  <div class="bg-slate-700 h-auto p-7 flex justify-center">
+    <div class="h-full md:flex p-4">
+      <SlideIn left>
+        <div class="my-auto md:px-5 px-3">
+          <p class="text-white italic text-xl mb-2">
+            Fluent in HTML, CSS, JavaScript, Laravel, Vue.js and Tailwind CSS
+          </p>
+          <hr />
+          <p class="text-white mb:p-0 py-2">
+            Proficient in creating responsive and dynamic user interfaces using
+            modern web development technologies
+          </p>
+        </div>
+      </SlideIn>
+      <SlideIn><img src="/photo-2.webp" alt="" class="h-64 mx-auto" /></SlideIn>
+    </div>
+  </div>
 </template>
 <script setup>
-import SlideIn from './SlideIn.vue'
+import SlideIn from "./SlideIn.vue";
 </script>
