3b39d3947240336399c986a6998d7fb891a395cb
Shauli Spitzer <sluahs@gmail.com>
2023-08-28 13:48:53 +0300

    made the slider only slide once per page load

--- src/components/SlideIn.vue
+++ src/components/SlideIn.vue
@@ -1,33 +1,41 @@
-<template><div ref="componentRef"  :class="['transition duration-1000 delay-100',  translateClass]">
-  <slot/>
-</div></template>
+<template>
+  <div
+    ref="componentRef"
+    :class="['transition duration-1000 delay-100', translateClass]"
+  >
+    <slot />
+  </div>
+</template>
 <script setup>
-import {ref, onMounted, computed} from 'vue';
+import { ref, onMounted, computed } from "vue";
 const props = defineProps({
-  left: Boolean
-})
-const showing = ref(false)
+  left: Boolean,
+});
+const showing = ref(false);
 const componentRef = ref(null);
+const hasBeenShown = ref(false);
 
 const observer = new IntersectionObserver((entries) => {
   const entry = entries[0];
-  showing.value = entry.isIntersecting;
-    });
-
+  if (!hasBeenShown.value && entry.isIntersecting) {
+    showing.value = true;
+    hasBeenShown.value = true; // Mark the component as shown once
+    observer.unobserve(componentRef.value); // Unobserve to prevent future triggers
+  }
+});
 
 onMounted(() => {
-      if (componentRef.value) {
-        observer.observe(componentRef.value);
-      }
-    });
+  if (componentRef.value) {
+    observer.observe(componentRef.value);
+  }
+});
 
 const translateClass = computed(() => {
-    if (!showing.value) {
-        if (props.left) {
-            return '-translate-x-52 opacity-0'
-        }
-        return 'translate-x-52 opacity-0'
+  if (!showing.value) {
+    if (props.left) {
+      return "-translate-x-52 opacity-0";
     }
-
+    return "translate-x-52 opacity-0";
+  }
 });
 </script>
